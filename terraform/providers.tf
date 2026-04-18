db85e652f691594e49148d4252cc322de02388f6
brian.perret <brian.perret@epita.fr>
2026-04-18 22:07:59 +0200

    feat: add vault + vso

---
+++ terraform/providers.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    keycloak = {
+      source  = "keycloak/keycloak"
+      version = "5.7.0"
+    }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = "5.6.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.16.0"
+    }
+  }
+}
+
+provider "keycloak" {
+  client_id                = "admin-cli"
+  url                      = var.keycloak_url
+  username                 = var.keycloak_username
+  password                 = var.keycloak_password
+  tls_insecure_skip_verify = false # Set to true if using self-signed certs
+}
+
+provider "vault" {
+  address = var.vault_url
+  # token   = "" # Set in environment variable VAULT_TOKEN or ~/.vault-token file (if both are unset)
+  # ca_cert_file = "" # Path to CA cert file if using self-signed certs
+}
+
+provider "cloudflare" {
+  # Configuration options
+}
---
+++ terraform/secret_templates.tf
@@ -0,0 +1,29 @@
+# Reference manual vars: https://developer.hashicorp.com/terraform/language/block/variable#specification
+
+# variable "fixme_var" {
+#   description = "An example of a var"
+#   type        = string
+#   sensitive   = true # Specifies if terraform hides this value in CLI output
+# }
+
+# Doc: https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/kv_secret_v2
+# resource "vault_kv_secret_v2" "fixme_secret" {
+#   mount               = vault_mount.kvv2.path
+#   name                = "fixme/path"
+#   cas                 = 1
+#   delete_all_versions = true
+#   data_json = jsonencode({
+#     FIXME_KEY = var.fixme_var
+#   })
+# }
+
+
+# -----------------------------------------------------------------------------
+# Cloudflare Secrets
+# -----------------------------------------------------------------------------
+
+variable "cloudflare_api_token_secret_var" {
+  description = "Cloudflare API token with permissions to manage DNS records."
+  type        = string
+  sensitive   = true
+}
